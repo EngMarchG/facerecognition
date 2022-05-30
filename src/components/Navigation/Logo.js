cb477d5aa895455a7e4fbb9247b622e573d310cd
EngMarchG <marchellino.ghorayeb@gmail.com>
2022-05-30 19:30:21 +0300

    Finalized

--- src/components/Navigation/Logo.js
+++ src/components/Navigation/Logo.js
@@ -5,9 +5,13 @@ import brain from './brain.png'
 
 const Logo = () => {
     return (
-        <div className="ma4 mt0">
-            <Tilt>
-                <div>
+        <div className="ma4 mt0 center">
+            <Tilt className="Tilt br2 shadow-2" options={{
+                tiltMaxAngleX:60,
+                tiltMaxAngleY:60
+            }}
+            style={{ height: 120, width: 120}}>
+                <div style={{ paddingTop: '15px'}}>
                     <img alt='logo' src={brain}/>
                 </div>
             </Tilt>
